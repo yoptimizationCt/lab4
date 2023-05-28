92c314d6539a58e772e38efb3a83f20a20fe8132
Ultimatereo <mansursult9@gmail.com>
2023-05-28 22:39:41 +0300

    Added batch and creating picture

--- SGD_modifications_from_2lab.py
+++ SGD_modifications_from_2lab.py
@@ -4,30 +4,30 @@ from paint_contour import paint_contour
 from create_function import create_function_XY
 
 eps_point = [20, 20]
-
+n = 100
 
 # j - точка, в которой мы берём производную
 # point - точка, которую мы подставляем в градиент
-def get_gradient(point, j):
+def get_gradient(point, j, X, Y):
     return np.array(
         [2 * X[j] * (point[0] * X[j] + point[1] - Y[j]),
          2 * (point[0] * X[j] + point[1] - Y[j])])
 
 
-def gradient(point, i, batch_size):
+def gradient(point, i, batch_size, X, Y):
     gr = np.zeros(len(point))
     number_of_coords = len(X)
     for j in range(batch_size):
-        gr += get_gradient(point, (i * batch_size + j) % number_of_coords)
+        gr += get_gradient(point, (i * batch_size + j) % number_of_coords, X, Y)
     return gr
 
 
-def SGD(lr=0.0009, epochs=100, batch_size=1):
+def SGD(X, Y, batch_size=1, lr=0.0009, epochs=100):
     points = np.zeros((epochs + 1, 2))
     num_epoch = epochs
     for epoch in range(1, epochs + 1):
         # j = np.random.randint(0, n)
-        gr = gradient(points[epoch - 1], epoch, batch_size)
+        gr = gradient(points[epoch - 1], epoch, batch_size, X, Y)
         if (abs(gr) < eps_point).all():
             num_epoch = epoch
             break
@@ -35,12 +35,12 @@ def SGD(lr=0.0009, epochs=100, batch_size=1):
     return points, num_epoch
 
 
-def SGD_with_momentum(lr=0.0009, v=np.zeros(2), gamma=0.5, epochs=100, batch_size=1):
+def SGD_with_momentum(X, Y, lr=0.0009, v=np.zeros(2), gamma=0.5, epochs=100, batch_size=1):
     points = np.zeros((epochs + 1, 2))
     num_epoch = epochs
     for epoch in range(1, epochs + 1):
         j = np.random.randint(0, n)
-        gr = gradient(points[epoch - 1], j, batch_size)
+        gr = gradient(points[epoch - 1], j, batch_size, X, Y)
         if (abs(gr) < eps_point).all():
             num_epoch = epoch
             break
@@ -49,12 +49,12 @@ def SGD_with_momentum(lr=0.0009, v=np.zeros(2), gamma=0.5, epochs=100, batch_siz
     return points, num_epoch
 
 
-def Nesterov(lr=0.0009, v=np.zeros(2), gamma=0.5, epochs=100, batch_size=1):
+def Nesterov(X, Y, lr=0.0009, v=np.zeros(2), gamma=0.5, epochs=100, batch_size=1):
     points = np.zeros((epochs + 1, 2))
     num_epoch = epochs
     for epoch in range(1, epochs + 1):
         j = np.random.randint(0, n)
-        gr = gradient(points[epoch - 1] - lr * gamma * v, j, batch_size)
+        gr = gradient(points[epoch - 1] - lr * gamma * v, j, batch_size, X, Y)
         if (abs(gr) < eps_point).all():
             num_epoch = epoch
             break
@@ -63,13 +63,13 @@ def Nesterov(lr=0.0009, v=np.zeros(2), gamma=0.5, epochs=100, batch_size=1):
     return points, num_epoch
 
 
-def AdaGrad(lr=1, epochs=100, eps=10e-8, batch_size=1):
+def AdaGrad(X, Y, lr=1, epochs=100, eps=10e-8, batch_size=1):
     state_sum = 0
     points = np.zeros((epochs + 1, 2))
     num_epoch = epochs
     for epoch in range(1, epochs + 1):
         j = np.random.randint(0, n)
-        gr = gradient(points[epoch - 1], j, batch_size)
+        gr = gradient(points[epoch - 1], j, batch_size, X, Y)
         if (abs(gr) < eps_point).all():
             num_epoch = epoch
             break
@@ -78,13 +78,13 @@ def AdaGrad(lr=1, epochs=100, eps=10e-8, batch_size=1):
     return points, num_epoch
 
 
-def RMS_prop(lr=0.1, epochs=100, alpha=0.5, eps=10e-8, batch_size=1):
+def RMS_prop(X, Y, lr=0.1, epochs=100, alpha=0.5, eps=10e-8, batch_size=1):
     points = np.zeros((epochs + 1, 2))
     s = 0
     num_epoch = epochs
     for epoch in range(1, epochs + 1):
         j = np.random.randint(0, n)
-        gr = gradient(points[epoch - 1], j, batch_size)
+        gr = gradient(points[epoch - 1], j, batch_size, X, Y)
         if (abs(gr) < eps_point).all():
             num_epoch = epoch
             break
@@ -93,14 +93,14 @@ def RMS_prop(lr=0.1, epochs=100, alpha=0.5, eps=10e-8, batch_size=1):
     return points, num_epoch
 
 
-def Adam(lr=0.1, epochs=100, eps=10e-8, beta1=0.9, beta2=0.999, batch_size=1):
+def Adam(X, Y, lr=0.1, epochs=100, eps=10e-8, beta1=0.9, beta2=0.999, batch_size=1):
     points = np.zeros((epochs + 1, 2))
     s = 0
     v = 0
     num_epoch = epochs
     for epoch in range(1, epochs + 1):
         j = np.random.randint(0, n)
-        gr = gradient(points[epoch - 1], j, batch_size)
+        gr = gradient(points[epoch - 1], j, batch_size, X, Y)
         if (abs(gr) < eps_point).all():
             num_epoch = epoch
             break
@@ -112,76 +112,76 @@ def Adam(lr=0.1, epochs=100, eps=10e-8, beta1=0.9, beta2=0.999, batch_size=1):
     return points, num_epoch
 
 
-def draw_line(points, file):
+def draw_line(points, file, X, Y, num=-1):
     plt.clf()
     plt.rcParams["figure.figsize"] = (10, 10)
     fig, ax = plt.subplots()
     ax.scatter(X, Y)
-    ax.plot([0, 10], [points[-1][1],
-                      10 * points[-1][0] + points[-1][1]], color='red', linewidth=5)
+    ax.plot([0, 10], [points[num][1],
+                      10 * points[num][0] + points[num][1]], color='red', linewidth=5)
     plt.savefig(file)
 
 
 def get_file(method, function, epoch_count, batch_size):
     return "tests/" + method + "/function(" + function + ")_epochs(" + str(epoch_count) + ")_batch(" + str(
         batch_size) + ")"
-
-
-n = 1000
-X = np.random.rand(n) * 10
-# epoch from number of batch
-number_of_epochs = {}
-epoch_count = 500
-batch_number = 30
-avg_number = [0 for i in range(batch_number + 1)]
-avg_number.pop(0)
-for a_exp in [3, 5, 7, 10, 20]:
-    for noise in [5]:
-        for lr in [1e-20, 1e-22, 1e-25]:
-            index = "function=" + str(a_exp) + "x;noise=" + str(noise) + ";lr=" + str(lr)
-            number_of_epochs[index] = [0 for _ in range(batch_number + 1)]
-            Y = a_exp * X + np.random.rand(n) * noise
-            for batch in range(1, batch_number + 1):
-                points_SGD = SGD(lr=0.0001, epochs=epoch_count, batch_size=batch)
-                # points_SGD_with_momentum = SGD_with_momentum(epochs=epoch_count)
-                # points_Nesterov = Nesterov(epochs=epoch_count)
-                # points_AdaGrad = AdaGrad(epochs=epoch_count)
-                # points_RMS_prop = RMS_prop(epochs=epoch_count)
-                # points_Adam = Adam(epochs=epoch_count)
-                number_of_epochs[index][batch] = points_SGD[1]
-            X_axis = [i for i in range(batch_number + 1)]
-            X_axis.pop(0)
-            count = 0
-            for j in range(len(number_of_epochs[index])):
-                if number_of_epochs[index][j] == epoch_count:
-                    count += 1
-            number_of_epochs[index].pop(0)
-            if count == 0:
-                for j in range(batch_number):
-                    avg_number[j] += number_of_epochs[index][j]
-                plt.clf()
-                plt.rcParams["figure.figsize"] = (10, 10)
-                fig, ax = plt.subplots()
-                # ax.set_title(index)
-                ax.set_xlabel("number_of_batches")
-                ax.set_ylabel("number_of_epochs")
-                plt.plot(X_axis, number_of_epochs[index], '-o')
-                plt.savefig("hehe1/" + index + ".png")
-for j in range(batch_number):
-    avg_number[j] /= len(number_of_epochs)
-X_axis = [i for i in range(batch_number + 1)]
-X_axis.pop(0)
-plt.clf()
-plt.rcParams["figure.figsize"] = (10, 10)
-fig, ax = plt.subplots()
-# ax.scatter(X_axis, avg_number)
-# ax.set_title(index)
-ax.set_xlabel("number_of_batches")
-ax.set_ylabel("number_of_epochs")
-avg_number[1] = avg_number[2] * 1.1
-avg_number[0] = avg_number[1] * 1.2
-plt.plot(X_axis, avg_number, '-o')
-plt.savefig("hehe1/overall.png")
+#
+#
+# n = 1000
+# X = np.random.rand(n) * 10
+# # epoch from number of batch
+# number_of_epochs = {}
+# epoch_count = 500
+# batch_number = 30
+# avg_number = [0 for i in range(batch_number + 1)]
+# avg_number.pop(0)
+# for a_exp in [3, 5, 7, 10, 20]:
+#     for noise in [5]:
+#         for lr in [1e-20, 1e-22, 1e-25]:
+#             index = "function=" + str(a_exp) + "x;noise=" + str(noise) + ";lr=" + str(lr)
+#             number_of_epochs[index] = [0 for _ in range(batch_number + 1)]
+#             Y = a_exp * X + np.random.rand(n) * noise
+#             for batch in range(1, batch_number + 1):
+#                 points_SGD = SGD(lr=0.0001, epochs=epoch_count, batch_size=batch)
+#                 # points_SGD_with_momentum = SGD_with_momentum(epochs=epoch_count)
+#                 # points_Nesterov = Nesterov(epochs=epoch_count)
+#                 # points_AdaGrad = AdaGrad(epochs=epoch_count)
+#                 # points_RMS_prop = RMS_prop(epochs=epoch_count)
+#                 # points_Adam = Adam(epochs=epoch_count)
+#                 number_of_epochs[index][batch] = points_SGD[1]
+#             X_axis = [i for i in range(batch_number + 1)]
+#             X_axis.pop(0)
+#             count = 0
+#             for j in range(len(number_of_epochs[index])):
+#                 if number_of_epochs[index][j] == epoch_count:
+#                     count += 1
+#             number_of_epochs[index].pop(0)
+#             if count == 0:
+#                 for j in range(batch_number):
+#                     avg_number[j] += number_of_epochs[index][j]
+#                 plt.clf()
+#                 plt.rcParams["figure.figsize"] = (10, 10)
+#                 fig, ax = plt.subplots()
+#                 # ax.set_title(index)
+#                 ax.set_xlabel("number_of_batches")
+#                 ax.set_ylabel("number_of_epochs")
+#                 plt.plot(X_axis, number_of_epochs[index], '-o')
+#                 plt.savefig("hehe1/" + index + ".png")
+# for j in range(batch_number):
+#     avg_number[j] /= len(number_of_epochs)
+# X_axis = [i for i in range(batch_number + 1)]
+# X_axis.pop(0)
+# plt.clf()
+# plt.rcParams["figure.figsize"] = (10, 10)
+# fig, ax = plt.subplots()
+# # ax.scatter(X_axis, avg_number)
+# # ax.set_title(index)
+# ax.set_xlabel("number_of_batches")
+# ax.set_ylabel("number_of_epochs")
+# avg_number[1] = avg_number[2] * 1.1
+# avg_number[0] = avg_number[1] * 1.2
+# plt.plot(X_axis, avg_number, '-o')
+# plt.savefig("hehe1/overall.png")
 
 # for a_exp in [1, 3, 5, 7, 10]:
 #     for noise in [1, 5, 10, 20, 30]:
--- SGD_pyTorch.py
+++ SGD_pyTorch.py
@@ -2,6 +2,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import os
+from SGD_modifications_from_2lab import *
 
 eps_point = [20, 20]
 modes = {'SGD': 0.0009, 'Momentum': 0.0009, 'Nesterov': 0.0009, 'Adam': 1, 'AdaGrad': 1, 'RMSProp': 0.1}
@@ -11,7 +13,16 @@ def array_to_torch(A):
     return torch.from_numpy(np.array([[a] for a in A])).float()
 
 
-def SGD_PT(X, Y, lr, mode='SGD', epochs=100, log=False, initial_guess=None):
+def get_mini_batch(X, Y, i, batch_size):
+    X_mini = []
+    Y_mini = []
+    for j in range(batch_size):
+        X_mini.append(X[(i * batch_size + j) % X.size()[0]].item())
+        Y_mini.append(Y[(i * batch_size + j) % Y.size()[0]].item())
+    return array_to_torch(X_mini), array_to_torch(Y_mini)
+
+
+def SGD_PT(X, Y, lr, mode='SGD', epochs=100, log=False, initial_guess=None, batch_size = 1):
     # Модель линейной регрессии
     # Если взять, что у нас Y = wX + b, то w = weight в нашей модели, а b - bias
     if initial_guess is None:
@@ -45,8 +56,10 @@ def SGD_PT(X, Y, lr, mode='SGD', epochs=100, log=False, initial_guess=None):
     num_epoch = epochs
     for epoch in range(epochs):
         # Подсчитываем ошибку модели
-        outputs = model(X)
-        loss = criterion(outputs, Y)
+
+        X_mini, Y_mini = get_mini_batch(X, Y, np.random.randint(0, X.size()[0]), batch_size)
+        outputs = model(X_mini)
+        loss = criterion(outputs, Y_mini)
 
         # Обнуляем градиент, если мы не хотим накапливать градиент
         optimizer.zero_grad()
@@ -57,7 +70,7 @@ def SGD_PT(X, Y, lr, mode='SGD', epochs=100, log=False, initial_guess=None):
         if abs(gradient_values[0]) < eps_point[0] and abs(gradient_values[1]) < eps_point[1]:
             print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch + 1, epochs, loss.item()))
             print('Predicted:', model.weight.item(), model.bias.item())
-            num_epoch = epoch + 1
+            num_epoch = epoch
             break
         # Делаем шаг градиентного спуска
         optimizer.step()
@@ -70,21 +83,38 @@ def SGD_PT(X, Y, lr, mode='SGD', epochs=100, log=False, initial_guess=None):
     return points, num_epoch
 
 
-n = 1000
+n = 100
 X = np.random.rand(n) * 10
 # epoch from number of batch
 number_of_epochs = {}
-epoch_count = 500
-batch_number = 30
+# epoch_count = 500
+# batch_number = 30
 for a_exp in [1, 3, 5, 7, 10]:
     for noise in [1, 5, 10, 20, 30]:
         Y = a_exp * X + np.random.rand(n) * noise
         funcStr = str(a_exp) + "x_noise(" + str(noise) + ")"
-        for mode in modes:
-            print(funcStr)
-            print("ЕБАТЬ ДА ЭТО ЖЕ ЧЕРТОВ", mode)
-            points = SGD_PT(array_to_torch(X),
-                            array_to_torch(Y), modes[mode], mode=mode, epochs=1000)
-            print(points[1])
-            print(points[0][:points[1]])
-            print('-----------')
+        for batch in [1, 10, 25, 50, 100]:
+            for epoch_count in [10, 100, 1000]:
+                # points_SGD, nSGD = SGD(X, Y, epochs=epoch_count, batch_size=batch)
+                # points_SGD_with_momentum, nMomentum = SGD_with_momentum(X, Y, epochs=epoch_count, batch_size=batch)
+                # points_Nesterov, nNesterov = Nesterov(X, Y, epochs=epoch_count, batch_size=batch)
+                # points_AdaGrad, nAdaGrad = AdaGrad(X, Y, epochs=epoch_count, batch_size=batch)
+                # points_RMS_prop, nRMS = RMS_prop(X, Y, epochs=epoch_count, batch_size=batch)
+                # points_Adam, nAdam = Adam(X, Y, epochs=epoch_count, batch_size=batch)
+                # draw_line(points_SGD, get_file("SGD", funcStr, epoch_count, batch), X, Y, nSGD)
+                # draw_line(points_SGD_with_momentum, get_file("Momentum", funcStr, epoch_count, batch), X, Y, nMomentum)
+                # draw_line(points_Nesterov, get_file("Nesterov", funcStr, epoch_count, batch), X, Y, nNesterov)
+                # draw_line(points_AdaGrad, get_file("AdaGrad", funcStr, epoch_count, batch), X, Y, nAdaGrad)
+                # draw_line(points_RMS_prop, get_file("RMSprop", funcStr, epoch_count, batch), X, Y, nRMS)
+                # draw_line(points_Adam, get_file("Adam", funcStr, epoch_count, batch), X, Y, nAdam)
+                for mode in modes:
+                    if not os.path.exists("tests/" + mode + "PyTorch"):
+                        os.makedirs("tests/" + mode + "PyTorch")
+                    print(funcStr)
+                    print("ЕБАТЬ ДА ЭТО ЖЕ ЧЕРТОВ", mode)
+                    points, num = SGD_PT(array_to_torch(X),
+                                    array_to_torch(Y), modes[mode], mode=mode, epochs=epoch_count, batch_size=batch)
+                    print(num)
+                    # print(points[:num + 1])
+                    draw_line(points, get_file(mode + "PyTorch", funcStr, epoch_count, batch), X, Y, num)
+                    print('-----------')
